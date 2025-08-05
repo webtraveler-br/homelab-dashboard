5cde9c6e9c72b9a95157e0bb0a4ac452459fff72
Lucas Prestes Mendes <mlucasprestes@gmail.com>
2025-08-04 21:19:30 -0300

    :hammer: latest insights controller

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -4,3 +4,6 @@ use Illuminate\Support\Facades\Route;
 
 // Rota para status de presença do gato na mesa (API)
 Route::get('/status/table-presence', [App\Http\Controllers\SensorLogController::class, 'latestPresence']);
+
+// Rota para os últimos insights
+Route::get('/insights/latest', [App\Http\Controllers\InsightController::class, 'latestInsights']);
